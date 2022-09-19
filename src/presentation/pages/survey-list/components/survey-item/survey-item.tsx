365c0c99ca901f1dd4e5bb400e7ac63b08637e0b
Leonardo <leonardo_tsuji@hotmail.com>
2022-09-19 19:22:14 -0300

    feat: ensure SurveyItem load icon and day correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -8,12 +8,13 @@ type Props = {
 }
 
 const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
+  const iconName = survey.didAnswer ? 'thumbUp' : 'thumbDown'
   return (
     <li className={styles.surveyItemWrap}>
       <div className={styles.surveyContent}>
-        <Icon iconName='thumbUp' className={styles.iconWrap}/>
+        <Icon iconName={iconName} className={styles.iconWrap}/>
         <time>
-          <span data-testid='day' className={styles.day}>{survey.date.getDate()}</span>
+          <span data-testid='day' className={styles.day}>{survey.date.getDate().toString().padStart(2, '0')}</span>
           <span data-testid='month' className={styles.month}>{survey.date.toLocaleDateString('pt-BR', { month: 'short' }).replace('.', '')}</span>
           <span data-testid='year' className={styles.year}>{survey.date.getFullYear()}</span>
         </time>
