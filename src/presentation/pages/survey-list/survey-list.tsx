075c6e3770f61805ad13519770472547004390e6
Leonardo <leonardo_tsuji@hotmail.com>
2022-09-19 19:58:40 -0300

    feat: ensure SurveyList render SurveyItems render error on failure

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -11,26 +11,35 @@ type Props = {
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
   const [state, setState] = useState({
-    surveys: [] as SurveyModel[]
+    surveys: [] as SurveyModel[],
+    error: ''
   })
 
   useEffect(() => {
     loadSurveyList.loadAll()
       .then(surveys => setState({
-        surveys
+        ...state, surveys
       }))
+      .catch(error => setState({ ...state, error: error.message }))
   }, [])
+
   return (
     <div className={styles.surveyListWrap}>
       <Header />
       <div className={styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul data-testid='survey-list'>
+        {state.error
+          ? <div>
+              <span data-testid='error'>{state.error}</span>
+              <button>Recarregar</button>
+            </div>
+          : <ul data-testid='survey-list'>
           {state.surveys.length
             ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey}/>)
             : <SurveyItemEmpty />
           }
-        </ul>
+        </ul>}
+
       </div>
       <Footer />
 
