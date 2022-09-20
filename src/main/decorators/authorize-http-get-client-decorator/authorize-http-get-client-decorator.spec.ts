30fffbe0a20000f8e637881c4e8d657ec1187808
Leonardo <leonardo_tsuji@hotmail.com>
2022-09-20 20:04:02 -0300

    test: ensure AuthorizeHttpGetClientDecorator merge token to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -23,13 +23,13 @@ const makeSut = (): SutTypes => {
 }
 
 describe('AuthorizeHttpGetClientDecorator', () => {
-  test('Shoul call GetStorage with correct value', async () => {
+  test('Should call GetStorage with correct value', async () => {
     const { sut, getStorageSpy } = makeSut()
     await sut.get(mockGetRequest())
     expect(getStorageSpy.key).toBe('account')
   })
 
-  test('Shoul not add headers if GetStorage is invalid', async () => {
+  test('Should not add headers if GetStorage is invalid', async () => {
     const { sut, httpGetClientSpy } = makeSut()
     const httpRequest: HttpGetParams = {
       url: faker.internet.url(),
@@ -42,7 +42,7 @@ describe('AuthorizeHttpGetClientDecorator', () => {
     expect(httpGetClientSpy.headers).toEqual(httpRequest.headers)
   })
 
-  test('Shoul add headers to HttpGetClient', async () => {
+  test('Should add headers to HttpGetClient', async () => {
     const { sut, getStorageSpy, httpGetClientSpy } = makeSut()
     getStorageSpy.value = mockAccountModel()
     const httpRequest: HttpGetParams = {
@@ -51,6 +51,24 @@ describe('AuthorizeHttpGetClientDecorator', () => {
     await sut.get(httpRequest)
     expect(httpGetClientSpy.url).toBe(httpRequest.url)
     expect(httpGetClientSpy.headers).toEqual({
+      'x-access-token': getStorageSpy.value.accessToken
+    })
+  })
+
+  test('Should merge headers to HttpGetClient', async () => {
+    const { sut, getStorageSpy, httpGetClientSpy } = makeSut()
+    getStorageSpy.value = mockAccountModel()
+    const field = faker.random.words()
+    const httpRequest: HttpGetParams = {
+      url: faker.internet.url(),
+      headers: {
+        field
+      }
+    }
+    await sut.get(httpRequest)
+    expect(httpGetClientSpy.url).toBe(httpRequest.url)
+    expect(httpGetClientSpy.headers).toEqual({
+      field,
       'x-access-token': getStorageSpy.value.accessToken
     })
   })
