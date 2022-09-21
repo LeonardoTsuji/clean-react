911c609bad1cb96952578c5c914096aea9fc5972
Leonardo <leonardo_tsuji@hotmail.com>
2022-09-21 19:47:15 -0300

    refactor: unused code

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,10 +1,7 @@
 import { AccountModel } from '@/domain/models'
-import { UnexpectedError } from '@/domain/errors'
-import { makeLocalStorageAdapter } from '../factories/cache'
+import { makeLocalStorageAdapter } from '@/main/factories/cache'
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
-  if (!account?.accessToken) throw new UnexpectedError()
-
   makeLocalStorageAdapter().set('account', account)
 }
 
