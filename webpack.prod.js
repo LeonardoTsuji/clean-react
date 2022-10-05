270609aa1a9eca4da798d44f753debe72a23f39f
Leonardo <leonardo_tsuji@hotmail.com>
2022-10-05 20:11:10 -0300

    feat: add favicon

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { DefinePlugin } = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 
@@ -30,7 +31,6 @@ module.exports = merge(common, {
     axios: 'axios',
     'react-dom': 'ReactDOM',
     'react-router-dom': 'ReactRouterDOM'
-
   },
   plugins: [
     new DefinePlugin({
@@ -41,6 +41,9 @@ module.exports = merge(common, {
     }),
     new MiniCssExtractPlugin({
       filename: 'main-bundle-[hash].css'
+    }),
+    new FaviconsWebpackPlugin({
+      logo: './public/favicon.png'
     })
   ]
 })
