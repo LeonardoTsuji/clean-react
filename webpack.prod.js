2b34cecbb0b83a2a88e51082b8f06e71ee951dc4
Leonardo <leonardo_tsuji@hotmail.com>
2022-10-04 21:44:42 -0300

    feat: github actions vercel

--- webpack.prod.js
+++ webpack.prod.js
@@ -34,7 +34,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new DefinePlugin({
-      'process.env.API_URL': JSON.stringify('http://fordevs.herokuapp.com/api')
+      'process.env.API_URL': JSON.stringify('https://fordevs.herokuapp.com/api')
     }),
     new HtmlWebpackPlugin({
       template: './template.prod.html'
