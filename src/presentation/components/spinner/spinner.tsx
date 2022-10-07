4396fdeeb839963a98b6f15f8ba16c082f3410a3
Leonardo <leonardo_tsuji@hotmail.com>
2022-10-07 18:56:11 -0300

    fix: change Spinner to remove DOM warning

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -5,8 +5,8 @@ interface Props extends React.HTMLAttributes<HTMLElement> {
   isNegative?: boolean
 }
 
-const Spinner: React.FC<Props> = (props: Props) => {
-  const negativeClass = props.isNegative ? styles.negative : ''
+const Spinner: React.FC<Props> = ({ isNegative, ...props }: Props) => {
+  const negativeClass = isNegative ? styles.negative : ''
   return (
     <div data-testid='spinner' {...props} className={[styles.spinner, negativeClass, props.className].join(' ')}>
       <div></div>
