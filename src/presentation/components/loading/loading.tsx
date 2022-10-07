3b38970441b079949763672149d253a8a1123d03
Leonardo <leonardo_tsuji@hotmail.com>
2022-10-07 18:27:28 -0300

    refactor: move Loading to its own file

---
+++ src/presentation/components/loading/loading.tsx
@@ -0,0 +1,16 @@
+import React, { memo } from 'react'
+import { Spinner } from '@/presentation/components'
+import styles from './loading-styles.scss'
+
+const Loading: React.FC = () => {
+  return (
+    <div className={styles.loadingWrap}>
+      <div className={styles.loading}>
+        <span >Aguarde...</span>
+        <Spinner isNegative/>
+      </div>
+    </div>
+  )
+}
+
+export default memo(Loading)
--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -1,13 +1,13 @@
 import React, { memo } from 'react'
-import Logo from '../logo/logo'
+import { Logo } from '@/presentation/components'
 import styles from './login-header-styles.scss'
 
 const LoginHeader: React.FC = () => {
   return (
     <header className={styles.headerWrap}>
-        <Logo />
-        <h1>4Dev - Enquetes para programadores</h1>
-      </header>
+      <Logo />
+      <h1>4Dev - Enquetes para programadores</h1>
+    </header>
   )
 }
 
