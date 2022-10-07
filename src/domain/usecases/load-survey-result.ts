d4a73843a2f5115fbc8c959b9009415b8710336f
Leonardo <leonardo_tsuji@hotmail.com>
2022-10-06 21:38:57 -0300

    feat: ensure RemoteLoadSurveyResult returns SurveyResultModel on 200

--- src/domain/usecases/load-survey-result.ts
+++ src/domain/usecases/load-survey-result.ts
@@ -2,17 +2,15 @@ export interface LoadSurveyResult {
   load: () => Promise<LoadSurveyResult.Model>
 }
 
-type LoadSurveyAnswer = {
-  image?: string
-  answer: string
-  count: number
-  percent: number
-}
-
 export namespace LoadSurveyResult {
   export type Model = {
     question: string
     date: Date
-    answers: LoadSurveyAnswer[]
+    answers: Array<{
+      image?: string
+      answer: string
+      count: number
+      percent: number
+    }>
   }
 }
