0e9c0a55e3f94c32aff15d7e6ce82d6aa01fc2a8
Leonardo <leonardo_tsuji@hotmail.com>
2022-10-09 21:33:37 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -7,19 +7,22 @@ import { SurveyResult } from '@/presentation/pages'
 import { ApiContext } from '@/presentation/contexts'
 import { mockAccountModel } from '@/domain/test'
 
+const makeSut = (): void => {
+  const history = createMemoryHistory({
+    initialEntries: ['/']
+  })
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
+      <Router location={history.location} navigator={history}>
+        <SurveyResult />
+      </Router>
+  </ApiContext.Provider>
+  )
+}
+
 describe('SurveyResult Component', () => {
   test('Should present correct initial state', () => {
-    const history = createMemoryHistory({
-      initialEntries: ['/']
-    })
-    const setCurrentAccountMock = jest.fn()
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock, getCurrentAccount: () => mockAccountModel() }}>
-        <Router location={history.location} navigator={history}>
-          <SurveyResult />
-        </Router>
-    </ApiContext.Provider>
-    )
+    makeSut()
     const surveyList = screen.getByTestId('survey-result')
     expect(surveyList.childElementCount).toBe(0)
     expect(screen.queryByTestId('error')).not.toBeInTheDocument()
