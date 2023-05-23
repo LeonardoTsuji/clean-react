94603d126defbe69d1d45a60a48334e6160a228f
Leonardo <leonardo_tsuji@hotmail.com>
2023-05-22 21:32:30 -0300

    feat: add SurveyResult composition

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import { makeLogin as MakeLogin, makeSignUp as MakeSignUp, makeSurveyList as MakeSurveyList } from '@/main/factories/pages'
+import { makeLogin as MakeLogin, makeSignUp as MakeSignUp, makeSurveyList as MakeSurveyList, makeSurveyResult as MakeSurveyResult } from '@/main/factories/pages'
 import { ApiContext } from '@/presentation/contexts'
 import { PrivateRoute } from '@/presentation/components'
 import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '../adapters/current-account-adapter'
-import { SurveyResult } from '@/presentation/pages'
 
 const Router: React.FC = () => {
   return (
@@ -19,7 +18,7 @@ const Router: React.FC = () => {
           <Route path='/login' element={<MakeLogin />}/>
           <Route path='/signup' element={<MakeSignUp />}/>
           <Route path='/' element={<PrivateRoute><MakeSurveyList /></PrivateRoute>}/>
-          <Route path='/surveys' element={<SurveyResult />}/>
+          <Route path='/surveys/:id' element={<PrivateRoute><MakeSurveyResult /></PrivateRoute>}/>
         </Routes>
       </BrowserRouter>
     </ApiContext.Provider>
