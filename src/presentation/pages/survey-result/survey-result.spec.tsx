2c7b54d3ba02bd2a9588ef27b6e14487ea1a9e65
Leonardo <leonardo_tsuji@hotmail.com>
2023-06-20 19:59:25 -0300

    test: ensure SurveyResult present new result on answer click

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -190,4 +190,40 @@ describe('SurveyResult Component', () => {
       expect(history.location.pathname).toBe('/login')
     })
   })
+
+  test('Should present SurveyResult data on SaveSurveyResult success', async () => {
+    const surveyResult = Object.assign(mockSurveyResultModel(), {
+      date: new Date('2023-06-20T00:00:00')
+    })
+    const saveSurveyResultSpy = new SaveSurveyResultSpy()
+    saveSurveyResultSpy.surveyResult = surveyResult
+    makeSut({ saveSurveyResultSpy })
+
+    await waitFor(async () => {
+      const answersWrap = screen.queryAllByTestId('answer-wrap')
+      fireEvent.click(answersWrap[1])
+
+      screen.getByTestId('survey-result')
+      expect(screen.getByTestId('day')).toHaveTextContent('20')
+      expect(screen.getByTestId('month')).toHaveTextContent('jun')
+      expect(screen.getByTestId('year')).toHaveTextContent('2023')
+      expect(screen.getByTestId('question')).toHaveTextContent(surveyResult.question)
+      expect(screen.getByTestId('answers').childElementCount).toBe(2)
+      expect(answersWrap[0]).toHaveClass('active')
+      expect(answersWrap[1]).not.toHaveClass('active')
+      const images = screen.queryAllByTestId('image')
+      expect(images[0]).toHaveAttribute('src', surveyResult.answers[0].image)
+      expect(images[0]).toHaveAttribute('alt', surveyResult.answers[0].answer)
+      expect(images[1]).toBeFalsy()
+      const answers = screen.queryAllByTestId('answer')
+      expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer)
+      expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer)
+      const percents = screen.queryAllByTestId('percent')
+      expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`)
+      expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`)
+    })
+    await waitFor(async () => {
+      expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+    })
+  })
 })
