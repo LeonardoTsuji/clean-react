4cd941aa69a225ce5b637ddc8861633f211b8cef
Leonardo <leonardo_tsuji@hotmail.com>
2023-06-22 19:22:05 -0300

    test: ensure SurveyResult logout on AccessDeniedError

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -19,6 +19,7 @@ describe('SurveyResult', () => {
         Helper.setLocalStorageItem('account', account)
       })
     })
+
     it('Should present error on UnexpectedError', () => {
       mockUnexpectedError()
       cy.visit('/surveys/any_id')
@@ -69,6 +70,7 @@ describe('SurveyResult', () => {
   })
   describe('save', () => {
     const mockUnexpectedError = (): void => Http.mockServerError(path, 'PUT')
+    const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'PUT')
 
     beforeEach(() => {
       cy.fixture('account').then(account => {
@@ -77,10 +79,17 @@ describe('SurveyResult', () => {
       mockLoadSuccess()
       cy.visit('/surveys/any_id')
     })
+
     it('Should present error on UnexpectedError', () => {
       mockUnexpectedError()
       cy.get('li:nth-child(2)').click()
       cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+    })
+
+    it('Should logout on AccessDeniedError', () => {
+      mockAccessDeniedError()
+      cy.get('li:nth-child(2)').click()
+      Helper.testUrl('/login')
     })
   })
 })
